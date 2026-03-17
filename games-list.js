48a8f85cf3551d5f0586fce38f6e50af95952dfa
Kevin <kevin@example.com>
2026-03-18 03:20:09 +0800

    重写: 完整HTML包含22张游戏图片和跑马灯下方营销入口

---
+++ games-list.js
@@ -0,0 +1,34 @@
+// 所有游戏图片列表
+const allGames = [
+    { name: 'Roma II', image: 'games/file_10---67636f32-dcbc-4379-aeae-45c7700b42f7.jpg' },
+    { name: 'Wild Bounty', image: 'games/file_11---ba216a0f-6b91-43e9-bd9b-5bedf7a8510f.jpg' },
+    { name: 'Mahjong Ways', image: 'games/file_12---e2427449-af5f-4956-9429-8cbdae4f194d.jpg' },
+    { name: 'Ways of the Qilin', image: 'games/file_13---72a6d871-b2cc-42e5-8ad0-ce7657706c00.jpg' },
+    { name: 'Wild Bandito', image: 'games/file_14---43f7367a-1a58-486a-98ce-1d9f231cad41.jpg' },
+    { name: 'Super Ace', image: 'games/file_15---4aaaa8e1-6430-4c3c-8864-21d1cdc8d0a4.jpg' },
+    { name: 'Guan Gong', image: 'games/file_16---aae1d744-6ad1-4767-8620-057f5a5627b1.jpg' },
+    { name: 'Caishen Wins', image: 'games/file_17---e4897b03-199d-4334-ab31-cf1ac174cec7.jpg' },
+    { name: 'Fortune Rabbit', image: 'games/file_18---fa91a84b-6f3f-4977-b987-000394ee18b4.jpg' },
+    { name: 'Prosperity Snake', image: 'games/file_19---8557538c-101b-4c0a-af08-bfb191bbfada.jpg' },
+    { name: 'Treasures of Aztec', image: 'games/file_20---5346dc14-8249-4099-bae9-bb21dec3f6b7.jpg' },
+    { name: 'Pinata Wins', image: 'games/file_21---393f43a6-12fd-46d3-ab35-33f86686aa4e.jpg' },
+    { name: 'Cocktail Nights', image: 'games/file_22---cd05c5c6-569e-4628-92ee-2eff51a0c628.jpg' },
+    { name: 'Fortune Ox', image: 'games/file_23---7cdf1f4d-4ef9-4181-96b3-2967fa19cfe9.jpg' },
+    { name: 'Ganesha Fortune', image: 'games/file_24---f8ad11bf-f994-4d2c-a1b2-865b385435a7.jpg' },
+    { name: 'Jurassic Kingdom', image: 'games/file_25---5d00d9c6-c2e2-432a-a1c4-5cbe953bea3f.jpg' },
+    { name: 'Big Caishen', image: 'games/file_26---0ee6fc80-d8a5-4086-8a96-6c4d0b0410ae.jpg' },
+    { name: 'Fortune Tiger', image: 'games/file_27---d4975b90-b03b-4d59-ae96-12345e734e3e.jpg' },
+    { name: 'Fortune Dragon', image: 'games/file_28---ae8f2329-9fb4-449f-beb5-cd77645f29f7.jpg' },
+    { name: 'Galaxy Miner', image: 'games/file_29---ebb35e5d-75a2-4422-8e13-460000954c87.jpg' },
+    { name: 'Gates of Olympus', image: 'games/file_30---931f01fa-9948-423c-b999-429099b89daa.jpg' },
+    { name: 'Wild Ape', image: 'games/file_31---c965d54c-78c2-4c3d-837f-e61d918c5348.jpg' }
+];
+
+// 营销活动入口（跑马灯下方）
+const marketingEntries = [
+    { name: 'Bônus Diário', icon: '🎁', color: '#ff6b6b' },
+    { name: 'Cashback', icon: '💰', color: '#4ecdc4' },
+    { name: 'VIP Club', icon: '👑', color: '#ffd93d' },
+    { name: 'Torneio', icon: '🏆', color: '#6c5ce7' },
+    { name: 'Indique', icon: '👥', color: '#a29bfe' }
+];
